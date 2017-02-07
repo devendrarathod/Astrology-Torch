e714347e2ab6604db49ff2f0df5c5fc836bddfd7
devendrarathod <devendrarathod23@gmail.com>
2017-02-07 14:35:18 -0800

    Initial Commit

---
+++ Astrology Torch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Astrology Torch
+//
+//  Created by Devendra Rathod on 2/7/17.
+//  Copyright © 2017 Devendra Rathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
